3e82ff2300e1157eaae2ca66a08a424d752f754e
Flora-M04 <ece.22becd60@silicon.ac.in>
2025-09-19 14:22:06 +0000

    basic python codes

--- prime2.py
+++ prime2.py
@@ -2,3 +2,4 @@ import math
 num = int(input("Enter a number: "))
 
 print("The number is prime" if num >1 and all(num%i !=0 for i in range(2, int(math.sqrt(num))+1)) else "The number is not prime")
+print("The number is not prime" if num <=1 else "")
